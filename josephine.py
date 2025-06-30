89859ab5837696736b0496907e31614bf8c4ef77
Josephine-gith <josephine.peronne@gmail.com>
2025-06-30 13:55:56 +0200

    df

--- josephine.py
+++ josephine.py
@@ -1,11 +1,17 @@
 import pandas as pd
+from listeindex import lis_index, lis_name
 
-xls = pd.ExcelFile('data/Fichier_donnees_ICP-MS_projets-Mines_2025.xls')
-df = pd.read_excel(xls, 'resultats_bruts_ICP-MS')
-#print(df.head())
+xls = pd.ExcelFile("data/Fichier_donnees_ICP-MS_projets-Mines_2025.xls")
+df = pd.read_excel(xls, "resultats_bruts_ICP-MS")
 
-df.dropna(axis=1, how='any', inplace=True)
+# Selection des données du DataFrame intéressantes
+df.dropna(axis=1, how="any", inplace=True)
+df = df[df["File:"].isin(lis_index)].reset_index(drop=True)
 
-df = df[df['File:'].isin(['Sample','Na / 23 [#3]'])].reset_index(drop=True)
+# Changement d'indexation
+df["File:"] = lis_name
+df.set_index(df["File:"], inplace=True)
+df.drop("File:", axis=1)
 
-print(df.head())
+
+print(df)
