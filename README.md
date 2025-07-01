46aeffa5fd461ca37126be2f36106a0a6ed33ae5
Josephine-gith <josephine.peronne@gmail.com>
2025-07-01 10:49:28 +0200

    readme et commentaire

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# hackaton_p9
+# Développement d’un logiciel de traitement de données de laboratoire
+## Hackaton 2025 - Mines Paris
+
+A quoi correspond les différents fichiers python :
+* listeindex.py : listes des éléments à considérer d'après la consigne, ainsi que le nouveau nom à leur attribuer, et le nom qui leur est donné dans le fichier de concentrations étalons.
+* donnees_en_df.py : code qui ouvre les fichiers excels, les met sous forme de DataFrame en ne gardant que les informations nécessaires à l'étude.
+* reglin_etalon.py : crée un dictionnaire avec les paramètres de régression linéraire entre le nombre de coûts et la concentration de chaque étalon, pour chaque élément.  
+
--- donnees_en_df.py
+++ donnees_en_df.py
@@ -1,38 +1,37 @@
+# Import de modules utiles
 import pandas as pd
+
+# Import des données
 from listeindex import lis_index, lis_name
 
 xls = pd.ExcelFile("data/Fichier_donnees_ICP-MS_projets-Mines_2025.xls")
 df = pd.read_excel(xls, "resultats_bruts_ICP-MS")
 
-# Selection des données du DataFrame intéressantes
+# Selection des données intéressantes du DataFrame
 df.dropna(axis=1, how="any", inplace=True)
 df = df[df["File:"].isin(lis_index)].reset_index(drop=True)
 
-# Changement d'indexation
+# Changement d'indexation du DataFrame
 df["File:"] = lis_name
 df.set_index(df["File:"], inplace=True)
 df.drop("File:", axis=1, inplace=True)
-i=0
-j=0
-numérotation=[]
-for k,elt in enumerate(df.loc['Sample']):
-    if elt == 'ET-DIL100-04-A':
-        i+=1
-        j=k
-    numérotation.append(i+(k-j)/100)
-print(numérotation)
-#print(df)
-df.loc['numérotation'] = numérotation
-df_blanc = df.loc[:, df.loc['Sample'] == 'HNO3 [0.37N]']
 
-df_int = df.loc[:, df.loc['Sample'] == 'InRe-A']
-                
-df_dil_bool = df.loc['Sample'].str.contains('DIL')
-df_dil = df.loc[:, df_dil_bool]
+# Numérotation des expériences dans l'ordre chronologique et par groupe
+i, j = 0, 0
+numérotation = []
+for k, elt in enumerate(df.loc["Sample"]):
+    if elt == "ET-DIL100-04-A":
+        i += 1
+        j = k
+    numérotation.append(i + (k - j) / 100)
+df.loc["numérotation"] = numérotation
 
-
-df_ech1 = df.drop(columns = df_dil.columns)
-df_ech2 = df_ech1.drop(columns = df_blanc.columns)
-df_ech = df_ech2.drop(columns = df_int.columns)
-
-print(df_dil)
+# Création de DataFrame séparés par 'Sample'
+# blancs, standards dilués, InRe (mesure de dérive d'appareil)
+df_blanc = df.loc[:, df.loc["Sample"] == "HNO3 [0.37N]"]
+df_dil = df.loc[:, df.loc["Sample"].str.contains("DIL")]
+df_InRe = df.loc[:, df.loc["Sample"] == "InRe-A"]
+# mesures de l'expérience
+df_ech_intermediaire1 = df.drop(columns=df_dil.columns)
+df_ech_intermediaire2 = df_ech_intermediaire1.drop(columns=df_blanc.columns)
+df_ech = df_ech_intermediaire2.drop(columns=df_InRe.columns)
