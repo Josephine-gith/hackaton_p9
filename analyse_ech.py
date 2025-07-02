fa5a854a24dea353e4e436e7e26fa60b705df0fc
Damien Faure <damien.faure42@gmail.com>
2025-07-02 11:02:04 +0200

    ok

--- analyse_ech.py
+++ analyse_ech.py
@@ -6,20 +6,23 @@ from correctionblancetsensib import dico_elt_corblancsensib
 
 df_concentration = df_ech.copy()
 a=0
-
+print(a)
 for val in df_concentration.loc['115In']:
-    coeffs = dico_elt_corblancsensib['115In'][int(df_dil.loc['numérotation_dilution'][a]) - 1]
-    df_concentration.loc['115In'][a] = coeffs[0] * val + coeffs[1]
+    coeffs = dico_elt_corblancsensib['115In'][int(np.array(df_ech.loc['numérotation_dilution'])[a]) - 1]
+    col_name = df_concentration.columns[a]
+    df_concentration.loc['115In', col_name] = coeffs[0] * val + coeffs[1]
+
     a+=1
 
 for i in range(1,len(df_concentration)):
     a=0
     for val in df_concentration.iloc[i]:
 
-        coeffs = dico_elt_corblancsensib[df_concentration.index[i]][int(df_dil.loc['numérotation_dilution'][a]) - 1]
-        
-        nb_coup_corrige= df_concentration.iloc[i]-df_blanc.loc[df_concentration.index[i]][int(df_dil.loc['numérotation_blanc'][a]) - 1]
-        nb_coup_corrigesensib = (nb_coup_corrige / (df_ech.loc['115In'][a]-df_blanc.loc['115In'][int(df_dil.loc['numérotation_blanc'][a]) - 1]))* df_concentration.loc['115Re'][a]
-        df_concentration.iloc[i][a] = coeffs[0] * nb_coup_corrigesensib + coeffs[1]
+        coeffs = dico_elt_corblancsensib[df_concentration.index[i]][int(np.array(df_ech.loc['numérotation_dilution'])[a]) - 1]
+
+        nb_coup_corrige= df_concentration.iloc[i]-np.array(df_blanc.loc[df_concentration.index[i]])[int(np.array(df_ech.loc['numérotation_blanc'])[a]) - 1]
+        nb_coup_corrigesensib = (nb_coup_corrige / (np.array(df_ech.loc['115In'])[a]-np.array(df_blanc.loc['115In'])[int(np.array(df_ech.loc['numérotation_blanc'])[a]) - 1]))* np.array(df_concentration.loc['115In'])[a]
+        df_concentration.iloc[i, a] = coeffs[0] * nb_coup_corrigesensib + coeffs[1]
         a+=1
-print(df_concentration)
+print(a)
+print(df_concentration, df_ech)
