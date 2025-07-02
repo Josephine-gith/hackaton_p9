8f94eab785e8b4bef8f14b23244a50957c8822f4
Josephine-gith <josephine.peronne@gmail.com>
2025-07-02 14:39:08 +0200

    interactif graphe ok

--- reglin_etalon_interactif.py
+++ reglin_etalon_interactif.py
@@ -1,12 +1,15 @@
+# Import des modules utiles
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Import des donnees
 from donnees_en_df import df_dil
 from liste_elements import lis_index_2, lis_name_clean
 
 xls = pd.ExcelFile("data/Fichier_traitement_donnees_ICP-MS_projets-Mines_20252.xls")
 
+# Variables globales
 labels = [
     "1ere mesures",
     "2eme mesures",
@@ -17,6 +20,7 @@ labels = [
 ]
 dico_elt = {}
 
+# Mise à jour des donnees
 df_facteur_dilution = pd.read_excel(xls, "indication_nom_echts", header=9)
 df_facteur_dilution.drop(["Unnamed: 2", "Unnamed: 3"], axis=1, inplace=True)
 df_etalon = pd.read_excel(xls, "solution-sdt_etalon", header=1)
@@ -38,46 +42,47 @@ for dil in [100, 30, 10, 3, 0]:
         df_etalon["Concentration étalon (ppb)"][:5]
     ) * np.array(temp["Facteur de dilution"])
 
+# Création des graphes
+fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+axes = axes.flatten()
 
 
-# Graphes 
-
-fig, axes = plt.subplots(1,1)
-
-# On stocke les données pour chaque scatter
 scatter_data = {}
 
-elt = "23Na"
-ax = axes
-dico_elt[elt] = []
+# Boucle pour faire un graphe pour chaque élement
+for idx, elt in enumerate(lis_name_clean):
+    ax = axes[idx]
+    dico_elt[elt] = []
 
-atome = "".join([c for c in elt if c.isalpha()])
-y = df_etalon.loc[atome].iloc[1:].to_numpy()
-y = np.array(y, dtype=float)
+    atome = "".join([c for c in elt if c.isalpha()])
+    y = df_etalon.loc[atome].iloc[1:].to_numpy()
+    y = np.array(y, dtype=float)
+    print(y)
+    # x = []
+    for i, label in enumerate(labels):
+        # i = 0
+        x = np.array(df_dil.loc[elt][i * 5 : (i + 1) * 5], dtype=float)
+        print(x)
+        sc = ax.scatter(x, y, label=label, picker=True)
+        # On stocke les coordonnées et l'objet scatter
+        scatter_data[sc] = {
+            "x": list(x),
+            "y": list(y),
+            "ax": ax,
+            "elt": elt,
+            "label": label,
+        }
 
-i = 0
-label = labels[i]
-x = np.array(df_dil.loc[elt][i * 5 : (i + 1) * 5], dtype=float)
-sc = ax.scatter(x, y, label=label, picker=True)
-# On stocke les coordonnées et l'objet scatter
-scatter_data[sc] = {
-    "x": list(x),
-    "y": list(y),
-    "ax": ax,
-    "elt": elt,
-    "label": label,
-}
+        # Régression initiale
+        coeffs = np.polyfit(x, y, 1)
+        dico_elt[elt].append(coeffs)
+        y_fit = np.polyval(coeffs, x)
+        (line,) = ax.plot(x, y_fit, "--")
+        scatter_data[ax] = {"line": line, "x": x, "y": y, "elt": elt}
 
-# Régression initiale
-coeffs = np.polyfit(x, y, 1)
-dico_elt[elt].append(coeffs)
-y_fit = np.polyval(coeffs, x)
-(line,) = ax.plot(x, y_fit, "--", color="red")
-scatter_data[ax] = {"line": line, "x": x, "y": y, "elt": elt}
-
-ax.set_title(elt)
-ax.grid()
-ax.legend()
+    ax.set_title(elt)
+    ax.grid()
+    ax.legend()
 
 fig.supylabel("Concentration (ppb)")
 fig.supxlabel("Nombre de coût")
@@ -99,6 +104,7 @@ def onpick(event):
     # Met à jour la régression
     if len(x) > 1:
         coeffs = np.polyfit(x, y, 1)
+        dico_elt[elt][0] = coeffs
         y_fit = np.polyval(coeffs, x)
         line = scatter_data[ax]["line"]
         line.set_data(x, y_fit)
@@ -108,4 +114,5 @@ def onpick(event):
 
 
 fig.canvas.mpl_connect("pick_event", onpick)
+
 plt.show()
