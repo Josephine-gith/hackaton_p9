99c2099c8787e4edfe255d88243a3648f7d60c98
Damien Faure <damien.faure42@gmail.com>
2025-07-03 11:26:42 +0200

    a

--- correctionblancetsensib.py
+++ correctionblancetsensib.py
@@ -119,70 +119,7 @@ plt.tight_layout()
 plt.show()
 
 
-# Régression linéaire et stock des coefficients dans un dictionnaire
-'''fig, axes = plt.subplots(1, 2, figsize=(8,4))
-for idx, elt in enumerate([ ("Re", "185Re")]):
-    ax = axes[idx]
-    dico_elt_corblancsensib[elt[1]] = []
 
-    y = df_InRe[f"{elt[0]} (ppm)"].iloc[::-1].to_numpy()
-    y=y*1000 # Conversion de ppm à ppb
-    y1 = df_InRe["In (ppm)"].iloc[::-1].to_numpy()
-    y1 = y1 * 1000  # Conversion de ppm à ppb
-
-    for i, label in enumerate(labels):
-        ligne = df_dil.loc[elt[1]]
-        indice_blanc = int(np.array(df_dil.loc['numérotation_blanc'])[i * 5]-1)
-        valeur_blanc = np.array(df_blanc.loc[elt[1]])[indice_blanc]
-        valeur_blancIn = np.array(df_blanc.loc['115In'])[indice_blanc]
-        x = np.array(df_dil.loc[elt[1]][i * 5 : (i + 1) * 5]- valeur_blanc)
-        x1 = np.array(df_dil.loc['115In'][i * 5 : (i + 1) * 5]- valeur_blancIn)
-        x = np.array((x/x1)*y1, dtype=float)
-        ax.scatter(x, y, label=label)
-
-        # Régression linéaire numpy
-        coeffs = np.polyfit(x, y, 1)
-        dico_elt_corblancsensib[elt[1]].append(coeffs)
-        y_fit = np.polyval(coeffs, x)
-        ax.plot(
-            x,
-            y_fit,
-            "--",
-            # label=f"(a={coeffs[0]:.2e}, b={coeffs[1]:.2e})",
-        )
-
-    ax.set_title(elt[0])
-    ax.grid()
-    ax.legend()
-
-for idx, elt in enumerate([("In", "115In")]):
-    ax = axes[idx]
-    dico_elt_corblancsensib[elt[1]] = []
-
-    y = df_InRe[f"{elt[0]} (ppm)"].iloc[::-1].to_numpy()
-    y= y * 1000  # Conversion de ppm à ppb
-    for i, label in enumerate(labels):
-        ligne = df_dil.loc[elt[1]]
-        indice_blanc = int(np.array(df_dil.loc['numérotation_blanc'])[i * 5]-1)
-        valeur_blanc = np.array(df_blanc.loc[elt[1]])[indice_blanc]
-        x = np.array(df_dil.loc[elt[1]][i * 5 : (i + 1) * 5]- valeur_blanc)
-        x = np.array(x, dtype=float)
-        ax.scatter(x, y, label=label)
-
-        # Régression linéaire numpy
-        coeffs = np.polyfit(x, y, 1)
-        dico_elt_corblancsensib[elt[1]].append(coeffs)
-        y_fit = np.polyval(coeffs, x)
-        ax.plot(
-            x,
-            y_fit,
-            "--",
-            # label=f"(a={coeffs[0]:.2e}, b={coeffs[1]:.2e})",
-        )
-
-    ax.set_title(elt[0])
-    ax.grid()
-    ax.legend()'''
 fig.supylabel("Concentration (ppb)")
 fig.supxlabel("Nombre de coût")
 plt.show()
